2489bf920b4699d754477b46d0790d5987055f76
T110360214 <t110360214@ntut.org.tw>
2021-10-24 00:43:53 +0800

    HW3

--- 110360214_沈育丞/4.15/source/modder.c
+++ 110360214_沈育丞/4.15/source/modder.c
@@ -8,16 +8,16 @@ int MODS1(float a,int b)
 	float w3 = a;
 	float w4 = a;
 	float w5 = a;
-	printf("1.10\t\t1.105\t\t1.110\t\t1.115\t\t1.120\n", w, w2, w3, w4, w5);
+	printf("years\t\t1.10\t\t1.105\t\t1.110\t\t1.115\t\t1.120\n", w, w2, w3, w4, w5);
 
-	for (int j=b ; j > 0 ; j--)
+	for (int j=1 ; j < 15 ; j++)
 	{
 		w *= 1.100;
 		w2 *= 1.105;
 		w3 *= 1.110;
 		w4 *= 1.115;
 		w5 *= 1.120;
-		printf("%.1f\t\t%.1f\t\t%.1f\t\t%.1f\t\t%.1f\n", w,w2,w3,w4,w5);
+		printf("%d\t\t%.1f\t\t%.1f\t\t%.1f\t\t%.1f\t\t%.1f\n",j, w,w2,w3,w4,w5);
 	}
 	
 
