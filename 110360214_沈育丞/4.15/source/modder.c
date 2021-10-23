81052907c8476935f86f668b47bb3cfa721e3ff1
T110360214 <t110360214@ntut.org.tw>
2021-10-24 00:40:47 +0800

    HW2

--- 110360214_沈育丞/4.15/source/modder.c
+++ 110360214_沈育丞/4.15/source/modder.c
@@ -3,14 +3,23 @@
 
 int MODS1(float a,int b)
 {
-	for (float i = 10; i < 12.5; i+=0.5)
+	float w = a;
+	float w2 = a;
+	float w3 = a;
+	float w4 = a;
+	float w5 = a;
+	printf("1.10\t\t1.105\t\t1.110\t\t1.115\t\t1.120\n", w, w2, w3, w4, w5);
+
+	for (int j=b ; j > 0 ; j--)
 	{
-		float w = a;
-		for (int j=b ; j > 0 ; j--)
-		{
-			w *= (1+(i / 100));
-		}
-		printf("total in rate %0.2f :%0.2f\n", i,w);
+		w *= 1.100;
+		w2 *= 1.105;
+		w3 *= 1.110;
+		w4 *= 1.115;
+		w5 *= 1.120;
+		printf("%.1f\t\t%.1f\t\t%.1f\t\t%.1f\t\t%.1f\n", w,w2,w3,w4,w5);
 	}
+	
+
 	return 0;
 }
